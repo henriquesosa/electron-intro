8dfb82cc0bb33b77846ccf2090cd3da9f7455763
Henrique Sosa <henriquesosa@gmail.com>
2016-01-06 18:07:47 -0200

    feat(main.js): Add new window features

--- src/main.js
+++ src/main.js
@@ -13,9 +13,26 @@ app.on('window-all-closed', function() {
 
 });
 
-app.on('ready', function() {
-	
-	mainWindow = new BrowserWindow({width: 800, height: 600});
+app.on('ready', getMainWindow);
+
+
+function getMainWindow() {
+	const workAreaSize = require('screen').getPrimaryDisplay().workAreaSize;
+
+	mainWindow = new BrowserWindow({
+			'web-preferences': {
+                'experimental-features': true,
+                'experimental-canvas-features': true,
+                'subpixel-font-scaling': true,
+                'overlay-scrollbars': true
+            },
+            resizable: true,
+            'min-width': 800,
+            'min-height': 600,
+            width: workAreaSize.width,
+            height: workAreaSize.height,
+            show: true
+    });
 	
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
 	
@@ -25,4 +42,4 @@ app.on('ready', function() {
 		
 		mainWindow = null;
 	});
-});
+}
