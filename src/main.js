96c54698b1bd338ce557e1768596efb4fbe90699
Henrique Sosa <henriquesosa@gmail.com>
2016-01-06 17:09:02 -0200

    refactor(src): Remove app folder

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ app.on('ready', function() {
 	
 	mainWindow = new BrowserWindow({width: 800, height: 600});
 	
-	mainWindow.loadURL('file://' + __dirname + '/src/index.html');
+	mainWindow.loadURL('file://' + __dirname + '/index.html');
 	
 	// mainWindow.openDevTools();
 	
